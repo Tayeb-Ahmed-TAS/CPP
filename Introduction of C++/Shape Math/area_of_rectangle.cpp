358d8237be8d91cdf17c7422317240b9b5585e2b
Tayeb Ahmed <atayeb.english939@gmail.com>
2023-04-12 20:24:07 +0600

    C++

--- Introduction of C++/Shape Math/area_of_rectangle.cpp
+++ Introduction of C++/Shape Math/area_of_rectangle.cpp
@@ -1,6 +1,8 @@
-// ! You are painting a rectangular wall and need to calculate the area in order
-// to buy the necessary amount of paint. ! The variables length and height
-// define the size of the wall. ! Calculate and output the area of the wall.
+// ! You are painting a rectangular wall and need to calculate the area in order to buy the necessary amount of paint.
+
+// ! The variables length and height define the size of the wall.
+
+// ! Calculate and output the area of the wall.
 
 #include <conio.h>
 #include <iostream>
