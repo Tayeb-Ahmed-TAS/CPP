47b71fb7606fc5f2ddda8f23ea50733707061fdf
Tayeb Ahmed <atayeb.english939@gmail.com>
2023-05-04 09:23:48 +0600

    C++

---
+++ Introduction of C++/Series/series1.cpp
@@ -0,0 +1,28 @@
+// ! 1 + (1 / 2) ^ 2 + (1 / 3) ^ 3 + ... + (1 / n) ^ n
+
+#include <conio.h>
+#include <iostream>
+#include <math.h>
+
+
+using namespace std;
+
+int main() {
+
+  float i, n, k, sum = 0;
+
+  cout << "Enter the last number => ";
+
+  cin >> n;
+
+  for (i = 1; i <= n; i++) {
+
+    k = pow((1 / i), i);
+
+    sum = sum + k;
+  }
+
+  cout << "The sum of the series is => " << sum;
+
+  getch();
+}
