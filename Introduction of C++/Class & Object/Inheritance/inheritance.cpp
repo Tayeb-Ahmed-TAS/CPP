ef1a4867599dc4c7328d47ab7fe9dedc174aac33
Tayeb Ahmed <atayeb.english939@gmail.com>
2023-08-02 19:44:28 +0600

    C++

--- Introduction of C++/Class & Object/Inheritance/inheritance.cpp
+++ Introduction of C++/Class & Object/Inheritance/inheritance.cpp
@@ -6,17 +6,19 @@ using namespace std;
 
 class Employee {
 
+public:
   int id;
 
   float salary;
 
-public:
   Employee(int inpId) {
 
     id = inpId;
 
     salary = 26.00;
   }
+
+  Employee() {} // ? Default Constructor
 
   void display() {
 
@@ -27,13 +29,25 @@ public:
 
 // ? Derived Class
 
+class Programmer : public Employee {
 
+public:
+  string language_name = "Python";
+
+  Programmer(int inpId) { id = inpId; }
+};
 
 int main() {
 
   Employee david(1), john(2), smith(3);
 
   david.display(), john.display(), smith.display();
+
+  Programmer skillF(4);
+
+  skillF.display();
+
+  cout << skillF.language_name << endl;
 
   return 0;
 }
