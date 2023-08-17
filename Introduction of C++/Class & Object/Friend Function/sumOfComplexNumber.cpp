6a91f5b0115940453c0e4f0aa8c9b49cf88c3990
Tayeb Ahmed <atayeb.english939@gmail.com>
2023-08-17 19:46:16 +0600

    C++

--- Introduction of C++/Class & Object/Friend Function/sumOfComplexNumber.cpp
+++ Introduction of C++/Class & Object/Friend Function/sumOfComplexNumber.cpp
@@ -1,12 +1,53 @@
 //! Note : 1 + 4i + 5 + 8i = 6 + 12i
 
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
+class Complex {
 
-    
+  int a, b;
 
-    return 0;
+public:
+  void setNumber(int n1, int n2) {
+
+    a = n1;
+
+    b = n2;
+  }
+
+  void printNumber() {
+
+    cout << "Your number is " << a << " + " << b << "i" << endl;
+  }
+
+  friend Complex sumComplex(Complex o1, Complex o2);
+};
+
+Complex sumComplex(Complex o1, Complex o2) {
+
+  Complex o3;
+
+  o3.setNumber((o1.a + o2.a), (o1.b + o2.b));
+
+  return o3;
+}
+
+int main() {
+
+  Complex c1, c2, sum;
+
+  c1.setNumber(1, 4);
+
+  c1.printNumber();
+
+  c2.setNumber(5, 8);
+
+  c2.printNumber();
+
+  sum = sumComplex(c1, c2);
+
+  sum.printNumber();
+
+  return 0;
 }
