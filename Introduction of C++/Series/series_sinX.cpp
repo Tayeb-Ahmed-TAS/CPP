23216f97dac16ca283153d5d0b30f3c391255eef
Tayeb Ahmed <atayeb.english939@gmail.com>
2023-09-12 13:46:23 +0600

    C++

--- Introduction of C++/Series/series_sinX.cpp
+++ Introduction of C++/Series/series_sinX.cpp
@@ -1,9 +1,53 @@
 // sinX = X - X^3 / 3! + X^5 / 5! - X^7 / 7! + ..........
 
+#include <cmath>
 #include <iostream>
+
+#define pi 3.1416
 
 using namespace std;
 
-#define PI 3.1416
+// ? Function to calculate factorial of a number
 
-int main() { return 0; }
+int fact(int n) {
+
+  int fact = 1;
+
+  for (int i = 1; i <= n; i++) {
+    fact = fact * i;
+  }
+
+  return fact;
+}
+
+// ? Function to calculate sinX using Taylor series
+
+double sin(double x, int n) {
+
+  double sum = x; // ? First sum is x
+
+  int sign = 1;
+
+  for (int i = 3; i <= n; i = i + 2) {
+    sign = sign * -1;
+
+    sum = sum + sign * pow(x, i) / fact(i);
+  }
+
+  return sum;
+}
+
+int main() {
+
+  double x, y = 30; // ? Replace with your desired angle in radians
+
+  x = y * (pi / 180); // ? Convert radian to degree
+
+  int n = 10; // ? Replace with the number of terms you want
+
+  double res = sin(x, n);
+
+  cout << "sin (" << y << ") = " << res << endl;
+
+  return 0;
+}
