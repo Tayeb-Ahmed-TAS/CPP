3063f4b8fdb814af52832481513fdadb1fde48aa
Tayeb Ahmed <atayeb.english939@gmail.com>
2023-09-12 13:54:52 +0600

    C++

---
+++ Introduction of C++/Series/series5.cpp
@@ -0,0 +1,20 @@
+// SUM = 1 + (1/2) ^ 2 + + (1/3) ^ 3 + + (1/4) ^ 4 + ... .... ...
+
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  float sum = 0;
+
+  for (int i = 1; i <= 4; i++) {
+
+    sum = sum + (1 / pow(i, i));
+  }
+
+  cout << "The sum of this series is " << sum;
+
+  return 0;
+}
