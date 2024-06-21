53eea82755f030f9c3868f80148faf48207cbafc
Tayeb Ahmed <atayeb.english939@gmail.com>
2024-06-21 13:35:26 +0600

    Interview Questions C++

--- Interview Questions/swapTwoNumbersWithoutThirdVariableAndAdditionAndSubtraction.cpp
+++ Interview Questions/swapTwoNumbersWithoutThirdVariableAndAdditionAndSubtraction.cpp
@@ -30,3 +30,29 @@ int main() {
 
   return 0;
 }
+
+/*
+
+Note:
+
+XOR operation between two bits results in 1 if the bits are different and 0 if
+they are the same.
+
+Let's break down the swap step by step:
+
+In the first line a = a ^ b;, a (3) XOR b (5) gives us 6 (binary: 0110 XOR 1010
+results in 0100). This value is stored back in a.
+
+In the second line b = a ^ b;, we XOR the new value of a (which is now 6) with
+the original value of b (5). This operation (6 XOR 5) gives 3 (binary: 0110 XOR
+1010 results in 0011). The result, 3, is stored in b.
+
+In the final line a = a ^ b;, we XOR the new value of a (6) with the new value
+of b (3). This operation (6 XOR 3) gives 5 (binary: 0110 XOR 0011 results in
+1010). The result, 5, is stored back in a.
+
+By the end of these three XOR operations, the value of a becomes the original
+value of b (5), and the value of b becomes the original value of a (3),
+effectively swapping their contents.
+
+*/
