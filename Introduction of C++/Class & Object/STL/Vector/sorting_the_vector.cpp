d111b1c4643e0fd3f83087f394003b1f72784b76
Tayeb Ahmed <atayeb.english939@gmail.com>
2024-07-14 11:18:33 +0600

    Vector

---
+++ Introduction of C++/Class & Object/STL/Vector/sorting_the_vector.cpp
@@ -0,0 +1,29 @@
+#include <algorithm> // for sort() function
+#include <iostream>
+#include <list>
+#include <vector>
+
+using namespace std;
+
+template <class t>
+
+void display(vector<t> vec) {
+  cout << "The elements of the vector are:" << endl;
+
+  for (int i = 0; i < vec.size(); i++) {
+    cout << vec[i] << " ";
+  }
+
+  cout << endl;
+}
+
+int main() {
+
+  vector<int> vec = {54, 5896, 16, 258, 26};
+
+  sort(vec.begin(), vec.end());
+
+  display(vec); // 16 26 54 258 5896
+
+  return 0;
+}
