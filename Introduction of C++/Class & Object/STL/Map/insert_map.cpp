0f2e435be0c067ce19a651c26586d35d04a8ceae
Tayeb Ahmed <atayeb.english939@gmail.com>
2024-07-14 13:45:45 +0600

    Map

---
+++ Introduction of C++/Class & Object/STL/Map/insert_map.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+
+  map<string, int> marks_map;
+
+  marks_map["Tina"] = 90;
+  marks_map["Rina"] = 80;
+  marks_map["Arnab"] = 70;
+  marks_map["Sina"] = 60;
+  marks_map["Harry"] = 50;
+
+  marks_map.insert({{"Rohan", 40}, {"Alex", 59}});
+
+  map<string, int>::iterator it;
+
+  for (it = marks_map.begin(); it != marks_map.end(); it++) {
+    cout << it->first << "\t" << it->second << endl;
+  }
+
+  return 0;
+}
